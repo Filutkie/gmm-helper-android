3d4a6dc10106cb17dfb1400fe83547144db72201
Filutkie <filutkie@gmail.com>
2015-09-08 22:33:16 +0300

    updated MyMarker model

--- app/src/main/java/com/filutkie/gmmhelper/model/MyMarker.java
+++ app/src/main/java/com/filutkie/gmmhelper/model/MyMarker.java
@@ -2,98 +2,87 @@ package com.filutkie.gmmhelper.model;
 
 import com.google.android.gms.maps.model.LatLng;
 
-public class MyMarker {
+public class MyMarker extends AbstractMarker{
 
+    private int id;
     private String title;
-
     private String description;
     private String address;
     private double latitude;
     private double longitude;
 
-    public MyMarker() {
+    public MyMarker id(int id) {
+        this.id = id;
+        return this;
     }
 
-    public MyMarker(String title, String description, String address, double latitude, double longitude) {
+    public MyMarker title(String title) {
         this.title = title;
+        return this;
+    }
+
+    public MyMarker description(String description) {
         this.description = description;
+        return this;
+    }
+
+    public MyMarker address(String address) {
         this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
+        return this;
     }
 
-    public MyMarker(String title, String description, double latitude, double longitude) {
-        this.title = title;
-        this.description = description;
+    public MyMarker latitude(double latitude) {
         this.latitude = latitude;
-        this.longitude = longitude;
+        return this;
     }
 
-    public MyMarker(String title, double latitude, double longitude) {
-
-        this.title = title;
-        this.latitude = latitude;
+    public MyMarker longitude(double longitude) {
         this.longitude = longitude;
+        return this;
     }
 
-    public MyMarker(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public double getLatitude() {
         return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
     }
 
     public double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
     public LatLng getPosition() {
-        return new LatLng(this.latitude, this.longitude);
+        return new LatLng(latitude, longitude);
     }
 
     @Override
     public String toString() {
         return "MyMarker{" +
-                "title='" + title + '\'' +
+                "id=" + id +
+                ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
                 ", address='" + address + '\'' +
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
                 '}';
     }
-}
 
+    @Override
+    public int getType() {
+        return TYPE_MARKER_DEFAULT;
+    }
+}
