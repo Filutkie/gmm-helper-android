e36954cf1ad6a1929fe54c986659dc17242e722e
Filutkie <filutkie@gmail.com>
2015-11-25 01:58:47 +0200

    implemented asynchronous marker loading and camera persistence

--- app/src/main/java/com/filutkie/gmmhelper/ui/GoogleMapFragment.java
+++ app/src/main/java/com/filutkie/gmmhelper/ui/GoogleMapFragment.java
@@ -3,16 +3,11 @@ package com.filutkie.gmmhelper.ui;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.LoaderManager;
 import android.content.ContentValues;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.location.Location;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -23,53 +18,41 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.filutkie.gmmhelper.R;
 import com.filutkie.gmmhelper.data.FeatureContract;
-import com.filutkie.gmmhelper.data.MyMarkerDatasource;
+import com.filutkie.gmmhelper.model.MarkerManager;
 import com.filutkie.gmmhelper.model.MyMarker;
-import com.filutkie.gmmhelper.utils.GeoUtils;
 import com.filutkie.gmmhelper.utils.PreferenceHelper;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
-import com.google.android.gms.maps.StreetViewPanorama;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.StreetViewPanoramaOrientation;
-import com.google.maps.android.MarkerManager;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class GoogleMapFragment extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor>,
         OnMapReadyCallback,
-        GoogleMap.OnMyLocationChangeListener,
-        GoogleMap.OnMapLongClickListener,
         GoogleMap.OnMarkerClickListener,
         GoogleMap.OnMapClickListener,
-        OnStreetViewPanoramaReadyCallback,
-        StreetViewPanorama.OnStreetViewPanoramaClickListener,
         SlidingUpPanelLayout.PanelSlideListener,
         View.OnClickListener {
 
     public static final String TAG = "GoogleMapFragment";
 
-    private MapFragment mapFragment;
     private GoogleMap map;
-    private StreetViewPanorama streetViewPanorama;
-    private MarkerManager markerManager;
 
     @Bind(R.id.sliding_layout)
     SlidingUpPanelLayout panelLayout;
@@ -83,15 +66,13 @@ public class GoogleMapFragment extends Fragment implements
     FloatingActionButton addMarkerButton;
     @Bind(R.id.fab_mylocation)
     FloatingActionButton mylocationButton;
+    @Bind(R.id.button_details)
+    Button details;
 
-    private MyMarkerDatasource markerDatasource;
-    private List<MyMarker> myMarkerList;
-    private HashMap<Marker, MyMarker> myMarkerHashMap;
-    private Marker tempMarker;
+    private MarkerManager markerManager;
 
     private int mapType = GoogleMap.MAP_TYPE_NORMAL;
-    private boolean isScreenRotated;
-    private boolean isSatellite;
+    private boolean isSatellite; // because map type is restored on orientation change
 
     private static final String KEY_MAP_TYPE = "key_map_type";
 
@@ -104,29 +85,22 @@ public class GoogleMapFragment extends Fragment implements
         panelLayout.setPanelSlideListener(this);
         addMarkerButton.setOnClickListener(this);
         mylocationButton.setOnClickListener(this);
+        details.setOnClickListener(this);
 
         FragmentManager fm = getChildFragmentManager();
-        mapFragment = (MapFragment) fm.findFragmentById(R.id.map_container);
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map_container);
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
             fm.beginTransaction().replace(R.id.map_container, mapFragment)
                     .commit();
         }
 
-        markerDatasource = new MyMarkerDatasource(getActivity());
         mapFragment.getMapAsync(this);
-
-//        StreetViewPanoramaFragment streetViewPanoramaFragment =
-//                (StreetViewPanoramaFragment) getFragmentManager()
-//                        .findFragmentById(R.id.fragment_sv);
-//        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
 
         if (savedInstanceState == null) {
             isSatellite = false;
-            isScreenRotated = false;
         } else {
             mapType = savedInstanceState.getInt(KEY_MAP_TYPE);
-            isScreenRotated = true;
         }
         return rootView;
     }
@@ -136,13 +110,17 @@ public class GoogleMapFragment extends Fragment implements
         super.onResume();
         map = ((MapFragment) getChildFragmentManager().findFragmentById(R.id.map_container)).getMap();
         map.setMyLocationEnabled(true);
-        markerManager = new MarkerManager(map);
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public void onPause() {
+        super.onPause();
         map.setMyLocationEnabled(false);
+        PreferenceHelper.saveLastCameraLatLngZoom(
+                getActivity(),
+                map.getCameraPosition().target.latitude,
+                map.getCameraPosition().target.longitude,
+                map.getCameraPosition().zoom);
     }
 
     @Override
@@ -150,37 +128,21 @@ public class GoogleMapFragment extends Fragment implements
         Log.d(TAG, "onMapReady");
         map = googleMap;
         googleMap.setMapType(mapType);
-        googleMap.setOnMyLocationChangeListener(this);
         googleMap.setOnMapClickListener(this);
         googleMap.setOnMarkerClickListener(this);
-        googleMap.setOnMapLongClickListener(this);
         googleMap.setMyLocationEnabled(true);
         googleMap.getUiSettings().setMyLocationButtonEnabled(false);
-        myMarkerList = markerDatasource.getAllMarkers();
-        myMarkerHashMap = new HashMap<>();
 
-        MarkerManager.Collection co = markerManager.newCollection("default");
-        for (MyMarker my : myMarkerList) {
-            Marker marker = co.addMarker(new MarkerOptions()
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
-                    .position(my.getPosition())
-                    .title(my.getTitle())
-                    .snippet(my.getDescription()));
-            myMarkerHashMap.put(marker, my);
-        }
-    }
+        markerManager = new MarkerManager(googleMap);
+        markerManager.setOnMarkerClickListener(this);
 
-    // TODO implement structure to manage custom markers
-    private Marker placeMarker(MyMarker myMarker) {
-        Marker marker = map.addMarker(
-                new MarkerOptions()
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
-                        .position(myMarker.getPosition())
-                        .title(myMarker.getTitle())
-                        .snippet(myMarker.getDescription())
-        );
-        myMarkerHashMap.put(marker, myMarker);
-        return marker;
+        CameraUpdate lastCameraPosition =
+                CameraUpdateFactory.newLatLngZoom(
+                        PreferenceHelper.getLastCameraLatLng(getActivity()),
+                        PreferenceHelper.getLastCameraZoom(getActivity()));
+        googleMap.moveCamera(lastCameraPosition);
+
+        getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
@@ -193,12 +155,7 @@ public class GoogleMapFragment extends Fragment implements
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_maptype_toggle:
-                if (map.getMapType() == GoogleMap.MAP_TYPE_TERRAIN) {
-                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                } else {
-                    map.setMapType(isSatellite ? GoogleMap.MAP_TYPE_HYBRID : GoogleMap.MAP_TYPE_NORMAL);
-                    isSatellite = !isSatellite;
-                }
+                toggleMapType();
                 break;
             case R.id.menu_maptype_terrain:
                 // prevent blinking on repetitive terrain item selecting
@@ -213,7 +170,7 @@ public class GoogleMapFragment extends Fragment implements
                 // TODO: if phone then:
 //                FragmentTransaction ft = getFragmentManager().beginTransaction();
 //                DialogFragment newFragment = MarkersDialogFragment.newInstance();
-//                ft.add(R.id.container, newFragment);
+//                ft.update(R.id.container, newFragment);
 //                ft.commit();
                 break;
             default:
@@ -228,62 +185,44 @@ public class GoogleMapFragment extends Fragment implements
         outState.putInt(KEY_MAP_TYPE, mapType);
     }
 
-    @Override
-    public void onMapClick(LatLng latLng) {
-        panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
-        if (tempMarker != null) {
-            tempMarker.setVisible(false);
+    private void toggleMapType() {
+        if (map.getMapType() == GoogleMap.MAP_TYPE_TERRAIN) {
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        } else {
+            isSatellite = !isSatellite;
+            map.setMapType(isSatellite ? GoogleMap.MAP_TYPE_HYBRID : GoogleMap.MAP_TYPE_NORMAL);
         }
     }
 
     @Override
-    public void onMapLongClick(LatLng latLng) {
-        String address = GeoUtils.getStringFromAddress(GeoUtils.getLocationAddress(getActivity(), latLng));
-        if (tempMarker != null) {
-            tempMarker.setVisible(false);
-        } else {
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(latLng)
-                    .icon(BitmapDescriptorFactory.fromBitmap(drawCircleMarkerIcon(Color.DKGRAY)))
-                    .anchor(0.5f, 0.5f)
-                    .title(address);
-            tempMarker = map.addMarker(markerOptions);
-        }
-        tempMarker.setPosition(latLng);
-        tempMarker.setTitle(address);
-        tempMarker.setSnippet("");
-        tempMarker.setVisible(true);
-        onMarkerClick(tempMarker);
+    public void onMapClick(LatLng latLng) {
+        panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
     }
 
     @Override
     public boolean onMarkerClick(final Marker marker) {
-        MyMarker myMarker = myMarkerHashMap.get(marker);
         map.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
-        if (myMarker != null) {
-            panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
-            titleTextView.setText(myMarker.getTitle());
-            descriptionTextView.setText(myMarker.getAddress());
-//            streetViewPanorama.setPosition(marker.getPosition(), 100);
-        } else if (marker.getTitle() != null) {
-            panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
-            titleTextView.setText(marker.getTitle());
-            descriptionTextView.setText(marker.getSnippet());
-        }
+        MyMarker myMarker = markerManager.getMyMarker(marker);
+
+        titleTextView.setText(myMarker.getTitle());
+        descriptionTextView.setText(myMarker.getDescription());
+
+        markerManager.onMarkerClick(marker);
+        panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
         return true;
     }
 
     @Override
     public void onClick(View v) {
-        LatLng mapCenter = map.getCameraPosition().target;
         switch (v.getId()) {
             case R.id.fab_marker_add:
+                LatLng mapCenter = map.getCameraPosition().target;
                 String title = "Marker " + PreferenceHelper.nextMarkerNumber(getActivity());
                 ContentValues values = new ContentValues();
                 values.put(FeatureContract.MarkerEntry.COLUMN_LATITUDE, mapCenter.latitude);
                 values.put(FeatureContract.MarkerEntry.COLUMN_LONGITUDE, mapCenter.longitude);
                 values.put(FeatureContract.MarkerEntry.COLUMN_TITLE, title);
-//                values.put(FeatureContract.MarkerEntry.COLUMN_TIME_ADDED, System.currentTimeMillis());
+                values.put(FeatureContract.MarkerEntry.COLUMN_TIME_ADDED, System.currentTimeMillis());
                 Uri uri = getActivity().getContentResolver().insert(FeatureContract.MarkerEntry.CONTENT_URI, values);
                 Log.d(TAG, "inserted row: " + uri);
                 MyMarker newMarker = new MyMarker()
@@ -291,7 +230,7 @@ public class GoogleMapFragment extends Fragment implements
                         .latitude(mapCenter.latitude)
                         .longitude(mapCenter.longitude)
                         .title(title);
-                placeMarker(newMarker);
+                markerManager.addMarker(newMarker);
                 break;
             case R.id.fab_mylocation:
                 if (map.getMyLocation() != null) {
@@ -305,47 +244,14 @@ public class GoogleMapFragment extends Fragment implements
                     }
                 }
                 break;
+            case R.id.button_details:
+                int markerId = markerManager.getMyMarker(
+                        markerManager.getCurrentSelection()).getId();
+                DialogFragment dialog = FeatureDetailDialogFragment.newInstance(markerId);
+                dialog.show(getFragmentManager(), "feature_dialog");
+
+                break;
         }
-    }
-
-    private Bitmap drawCircleMarkerIcon(int color) {
-        int wheight = 25;
-        final Bitmap output = Bitmap.createBitmap(wheight, wheight, Bitmap.Config.ARGB_8888);
-        final Canvas canvas = new Canvas(output);
-        int col = Color.RED;
-        if (color != 0) {
-            col = color;
-        }
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, wheight, wheight);
-        final RectF rectF = new RectF(rect);
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(col);
-        canvas.drawOval(rectF, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        return output;
-    }
-
-    @Override
-    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
-        streetViewPanorama.setOnStreetViewPanoramaClickListener(this);
-        this.streetViewPanorama = streetViewPanorama;
-    }
-
-    @Override
-    public void onStreetViewPanoramaClick(StreetViewPanoramaOrientation streetViewPanoramaOrientation) {
-        Toast.makeText(getActivity(), "Pano clicked.", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onMyLocationChange(Location location) {
-        Log.d(TAG, "onMyLocationChange");
-        if (!isScreenRotated) {
-            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-        }
-        map.setOnMyLocationChangeListener(null);
     }
 
     @Override
@@ -354,24 +260,51 @@ public class GoogleMapFragment extends Fragment implements
 
     @Override
     public void onPanelExpanded(View panel) {
-        Log.i(TAG, "onPanelExpanded");
     }
 
     @Override
     public void onPanelCollapsed(View panel) {
-        Log.i(TAG, "onPanelCollapsed");
     }
 
     @Override
     public void onPanelAnchored(View panel) {
-        Log.i(TAG, "onPanelAnchored");
     }
 
     @Override
     public void onPanelHidden(View panel) {
-        Log.i(TAG, "onPanelHidden");
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = FeatureContract.MarkerEntry.CONTENT_URI;
+        String[] projection = null;
+        return new CursorLoader(getActivity(),
+                uri,
+                projection,
+                null,
+                null,
+                null);
+    }
 
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        List<MyMarker> myMarkerList = new ArrayList<>();
+        MyMarker myMarker;
+        Log.d(TAG, data.getCount()+"");
+        for(data.moveToFirst(); !data.isAfterLast(); data.moveToNext()) {
+            myMarker = new MyMarker()
+                    .id(data.getInt(data.getColumnIndex(FeatureContract.MarkerEntry._ID)))
+                    .title(data.getString(data.getColumnIndex(FeatureContract.MarkerEntry.COLUMN_TITLE)))
+                    .description(data.getString(data.getColumnIndex(FeatureContract.MarkerEntry.COLUMN_DESCRIPTION)))
+                    .latitude(data.getDouble(data.getColumnIndex(FeatureContract.MarkerEntry.COLUMN_LATITUDE)))
+                    .longitude(data.getDouble(data.getColumnIndex(FeatureContract.MarkerEntry.COLUMN_LONGITUDE)));
+            myMarkerList.add(myMarker);
+        }
+        markerManager.addAllMarkers(myMarkerList);
+    }
 
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/filutkie/gmmhelper/utils/PreferenceHelper.java
+++ app/src/main/java/com/filutkie/gmmhelper/utils/PreferenceHelper.java
@@ -3,9 +3,14 @@ package com.filutkie.gmmhelper.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class PreferenceHelper {
 
     private static final String KEY_PREF_MARKER_NUMBER = "marker_number";
+    private static final String KEY_PREF_LAST_LATITUDE = "last_latitude";
+    private static final String KEY_PREF_LAST_LONGITUDE = "last_longitude";
+    private static final String KEY_PREF_LAST_ZOOM = "last_zoom";
 
     public static int nextMarkerNumber(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
@@ -13,4 +18,20 @@ public class PreferenceHelper {
         return prefs.getInt(KEY_PREF_MARKER_NUMBER, 0);
     }
 
+    public static LatLng getLastCameraLatLng(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+        return new LatLng(prefs.getFloat(KEY_PREF_LAST_LATITUDE, 0.0f), prefs.getFloat(KEY_PREF_LAST_LONGITUDE, 0.0f));
+    }
+
+    public static float getLastCameraZoom(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+        return prefs.getFloat(KEY_PREF_LAST_ZOOM, 0);
+    }
+
+    public static void saveLastCameraLatLngZoom(Context context, double latitude, double longitude, double zoom) {
+        SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+        prefs.edit().putFloat(KEY_PREF_LAST_LATITUDE, (float) latitude).apply();
+        prefs.edit().putFloat(KEY_PREF_LAST_LONGITUDE, (float) longitude).apply();
+        prefs.edit().putFloat(KEY_PREF_LAST_ZOOM, (float) zoom).apply();
+    }
 }
